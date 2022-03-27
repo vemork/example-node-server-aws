c52cc545693cd87cafc9bd3cda079eb88cdaf62f
Emmanuel.Rubio <Emmanuel.Rubio@vtex.com.br>
2022-03-27 15:26:29 -0500

    adding express

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
-const http = require('http')
+const express = require('express')
 
-const server = http.createServer((req, res) => {
-  res.end('Hello world!')
+const app = express()
+
+app.get('/', (req, res)=>{
+  res.send('Hello world with Express')
 })
 
-server.listen(3000)
+app.listen(3000)
 console.log('Server running on port 3000 :>> ');
