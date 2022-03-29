87181fe9db752ade1db7bb67dbc01dd88f1500df
Emmanuel.Rubio <Emmanuel.Rubio@vtex.com.br>
2022-03-28 22:50:21 -0500

    small adjust

--- server.js
+++ server.js
@@ -6,5 +6,5 @@ app.get('/mutant/', (req, res)=>{
   res.send('Hello world with Express')
 })
 
-// app.listen(3000)
-// console.log('Server running on port 3000 :>> ');
+app.listen(3000)
+console.log('Server running on port 3000 :>> ');
