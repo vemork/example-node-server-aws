8c101fd5d2841f05d751b6b7c29d222e660cfddb
Emmanuel.Rubio <Emmanuel.Rubio@vtex.com.br>
2022-03-30 01:58:18 -0500

    mongo validation

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+import mongoose from "mongoose";
 import express from "express";
 
 import router from "./Routes/routes.js";
@@ -5,6 +6,16 @@ import router from "./Routes/routes.js";
 const app = express();
 app.use(express.json());
 app.use(router);
+
+const uri = "mongodb://3.89.218.67/test";
+const options = {};
+
+mongoose
+.connect(uri, options)
+.then(() => {
+  console.log("Database connected ");
+})
+.catch((err) => console.log("err :>> ", err));
 
 app.listen(3000);
 console.log("Server running on port 3000 :>> ");
