216aa6a1e4e052b144e4b94305be5fc121cfae17
Emmanuel.Rubio <Emmanuel.Rubio@vtex.com.br>
2022-03-30 02:40:46 -0500

    isolate database

---
+++ database.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const uri = "mongodb://my_user:my_pwd@localhost:27017/mern";
+const options = {};
+
+mongoose
+  .connect(uri, options)
+  .then(() => {
+    console.log("Database connected ");
+  })
+  .catch((err) => console.log("err :>> ", err));
+
+//mongo
+// show databases = show dbs
+// use test (database name)
+// show collections
+// db.Users.find()
+// db.dropDatabase()
--- server.js
+++ server.js
@@ -1,21 +1,10 @@
-import mongoose from "mongoose";
-import express from "express";
+import express  from "express";
 
-import router from "./Routes/routes.js";
+import  router  from "./Routes/routes.js";
 
 const app = express();
 app.use(express.json());
 app.use(router);
-
-const uri = "mongodb://3.89.218.67:27017/test";
-const options = {};
-
-mongoose
-.connect(uri, options)
-.then(() => {
-  console.log("Database connected ");
-})
-.catch((err) => console.log("err :>> ", err));
 
 app.listen(3000);
 console.log("Server running on port 3000 :>> ");
